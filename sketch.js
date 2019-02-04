b336f3bd5e0a706b6d6e1cd2b30b7d703d6e5973
Naukhez Ali <naukhez.3@hotmail.co.uk>
2019-02-04 19:55:54 +0000

    Testing draw functions

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() {
   // put setup code here
+  createCanvas(1000, 800);
 }
 
 function draw() {
   // put drawing code here
+
+
+  if(mouseIsPressed){
+    fill(0);
+  } else {
+    fill(244);
+  }
+
+  ellipse(mouseX, mouseY, 80, 80);
 }
